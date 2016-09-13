08444dfdfe6216eb9b1ec6860fffaf5c135d820b
Zhe Zhang <zzxuanyuan@gmail.com>
2016-09-12 21:27:08 -0500

    Implement 1*4, 1*8, 4*1, 8*1 multiplications and particularly interchanging outer two loops could improve the performance for 1*8

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -21,7 +21,8 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 
 #define min(a,b) (((a)<(b))?(a):(b))
 
-static void fourrow_mult (int K, double *x, int incx, double *y, double *frm)
+/* 4 rows of x multiply 1 column of y */
+static void mult4x1 (int K, int inc, double *x, double *y, double *frm)
 {
 /* frm represents four row multiplication
  *  frm[0] = x[0] * y + frm[0]
@@ -43,10 +44,10 @@ static void fourrow_mult (int K, double *x, int incx, double *y, double *frm)
   for(int k = 0; k < K; ++k)
   {
     y_reg      = y[k];
-    frm_reg_0 += y_reg * x[k*incx];
-    frm_reg_1 += y_reg * x[1+k*incx];
-    frm_reg_2 += y_reg * x[2+k*incx];
-    frm_reg_3 += y_reg * x[3+k*incx];
+    frm_reg_0 += y_reg * x[k*inc];
+    frm_reg_1 += y_reg * x[1+k*inc];
+    frm_reg_2 += y_reg * x[2+k*inc];
+    frm_reg_3 += y_reg * x[3+k*inc];
   }
 
   frm[0] = frm_reg_0;
@@ -55,7 +56,42 @@ static void fourrow_mult (int K, double *x, int incx, double *y, double *frm)
   frm[3] = frm_reg_3;
 }
 
-static void eightrow_mult (int K, double *x, int incx, double *y, double *erm)
+/* 1 row of x multiplies 4 columns of y */
+static void mult1x4 (int K, int inc, double *x, double *y, double *fcm)
+{
+  register double fcm_reg_0 = 0.0;
+  register double fcm_reg_1 = 0.0;
+  register double fcm_reg_2 = 0.0;
+  register double fcm_reg_3 = 0.0;
+  register double x_reg     = 0.0;
+  fcm_reg_0  = fcm[0];
+  fcm_reg_1  = fcm[inc];
+  fcm_reg_2  = fcm[2*inc];
+  fcm_reg_3  = fcm[3*inc];
+  double *x_ptr  = &x[0];
+  double *y0_ptr = &y[0];
+  double *y1_ptr = &y[inc];
+  double *y2_ptr = &y[2*inc];
+  double *y3_ptr = &y[3*inc];
+
+  for(int k = 0; k < K; ++k)
+  {
+    x_reg      = *x_ptr;
+    x_ptr     += inc;
+    fcm_reg_0 += x_reg * *y0_ptr++;
+    fcm_reg_1 += x_reg * *y1_ptr++;
+    fcm_reg_2 += x_reg * *y2_ptr++;
+    fcm_reg_3 += x_reg * *y3_ptr++;
+  }
+
+  fcm[0]     = fcm_reg_0;
+  fcm[inc]   = fcm_reg_1;
+  fcm[2*inc] = fcm_reg_2;
+  fcm[3*inc] = fcm_reg_3;
+}
+
+/* 8 rows of x multiply 1 column of y */
+static void mult8x1 (int K, int inc, double *x, double *y, double *erm)
 {
 /* similar with above, calculate eight rows at each iteration */
   register double erm_reg_0 = 0.0;
@@ -79,14 +115,14 @@ static void eightrow_mult (int K, double *x, int incx, double *y, double *erm)
   for(int k = 0; k < K; ++k)
   {
     y_reg      = y[k];
-    erm_reg_0 += y_reg * x[k*incx];
-    erm_reg_1 += y_reg * x[1+k*incx];
-    erm_reg_2 += y_reg * x[2+k*incx];
-    erm_reg_3 += y_reg * x[3+k*incx];
-    erm_reg_4 += y_reg * x[4+k*incx];
-    erm_reg_5 += y_reg * x[5+k*incx];
-    erm_reg_6 += y_reg * x[6+k*incx];
-    erm_reg_7 += y_reg * x[7+k*incx];
+    erm_reg_0 += y_reg * x[k*inc];
+    erm_reg_1 += y_reg * x[1+k*inc];
+    erm_reg_2 += y_reg * x[2+k*inc];
+    erm_reg_3 += y_reg * x[3+k*inc];
+    erm_reg_4 += y_reg * x[4+k*inc];
+    erm_reg_5 += y_reg * x[5+k*inc];
+    erm_reg_6 += y_reg * x[6+k*inc];
+    erm_reg_7 += y_reg * x[7+k*inc];
   }
 
   erm[0] = erm_reg_0;
@@ -99,19 +135,73 @@ static void eightrow_mult (int K, double *x, int incx, double *y, double *erm)
   erm[7] = erm_reg_7;
 }
 
+/* 1 row of x multiplies 8 columns of y */
+static void mult1x8 (int K, int inc, double *x, double *y, double *ecm)
+{
+  register double ecm_reg_0 = 0.0;
+  register double ecm_reg_1 = 0.0;
+  register double ecm_reg_2 = 0.0;
+  register double ecm_reg_3 = 0.0;
+  register double ecm_reg_4 = 0.0;
+  register double ecm_reg_5 = 0.0;
+  register double ecm_reg_6 = 0.0;
+  register double ecm_reg_7 = 0.0;
+  register double x_reg     = 0.0;
+  ecm_reg_0  = ecm[0];
+  ecm_reg_1  = ecm[inc];
+  ecm_reg_2  = ecm[2*inc];
+  ecm_reg_3  = ecm[3*inc];
+  ecm_reg_4  = ecm[4*inc];
+  ecm_reg_5  = ecm[5*inc];
+  ecm_reg_6  = ecm[6*inc];
+  ecm_reg_7  = ecm[7*inc];
+  double *x_ptr  = &x[0];
+  double *y0_ptr = &y[0];
+  double *y1_ptr = &y[inc];
+  double *y2_ptr = &y[2*inc];
+  double *y3_ptr = &y[3*inc];
+  double *y4_ptr = &y[4*inc];
+  double *y5_ptr = &y[5*inc];
+  double *y6_ptr = &y[6*inc];
+  double *y7_ptr = &y[7*inc];
+
+  for(int k = 0; k < K; ++k)
+  {
+    x_reg      = *x_ptr;
+    x_ptr     += inc;
+    ecm_reg_0 += x_reg * *y0_ptr++;
+    ecm_reg_1 += x_reg * *y1_ptr++;
+    ecm_reg_2 += x_reg * *y2_ptr++;
+    ecm_reg_3 += x_reg * *y3_ptr++;
+    ecm_reg_4 += x_reg * *y4_ptr++;
+    ecm_reg_5 += x_reg * *y5_ptr++;
+    ecm_reg_6 += x_reg * *y6_ptr++;
+    ecm_reg_7 += x_reg * *y7_ptr++;
+  }
+
+  ecm[0]     = ecm_reg_0;
+  ecm[inc]   = ecm_reg_1;
+  ecm[2*inc] = ecm_reg_2;
+  ecm[3*inc] = ecm_reg_3;
+  ecm[4*inc] = ecm_reg_4;
+  ecm[5*inc] = ecm_reg_5;
+  ecm[6*inc] = ecm_reg_6;
+  ecm[7*inc] = ecm_reg_7;
+}
+
 /* This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
 static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
   /* For each row i of A */
-  for (int i = 0; i < M; i+=8)
+  for (int j = 0; j < N; j+=8)
     /* For each column j of B */ 
-    for (int j = 0; j < N; ++j) 
+    for (int i = 0; i < M; ++i) 
     {
       /* Compute C(i,j) */
-//      fourrow_mult(K, A+i, lda, B+j*lda, C+i+j*lda);
-      eightrow_mult(K, A+i, lda, B+j*lda, C+i+j*lda);
+//      mult1x4(K, lda, A+i, B+j*lda, C+i+j*lda);
+      mult1x8(K, lda, A+i, B+j*lda, C+i+j*lda);
     }
 }
 
